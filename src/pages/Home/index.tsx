9e9862d7dc72ba2b4a7e1132f0d75744560bf5fa
Conrado Alexsander Soares Gonçalves <53683786+conradoalexsander@users.noreply.github.com>
2023-05-05 15:18:18 +0200

    Added App styles

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,51 @@
+import { Play } from "phosphor-react";
+import {
+    CountDownContainer,
+    FormContainer,
+    HomeContainer,
+    MinutesAmountInput,
+    Separator,
+    StartCountdownButton,
+    TaskInput,
+} from "./styles";
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <div>
+            <label htmlFor="task">I will work in</label>
+            <TaskInput
+              id="task"
+              type="text"
+              placeholder="Give a nice name to your project"
+            />
+
+            <label htmlFor="minutesAmount">while</label>
+            <MinutesAmountInput
+              id="minutesAmount"
+              type="number"
+              placeholder="00"
+            />
+
+            <span>minutes.</span>
+          </div>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <StartCountdownButton type="submit">
+          <Play size={24} />
+          Start
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  );
+}
